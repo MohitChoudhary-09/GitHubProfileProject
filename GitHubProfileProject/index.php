819a04620e0465bc0a7470f43b932f46636a35e6
Mohit Choudhary <121396088+MohitChoudhary-09@users.noreply.github.com>
2023-07-18 14:26:36 +0530

    Update index.php

--- GitHubProfileProject/index.php
+++ GitHubProfileProject/index.php
@@ -8,25 +8,52 @@
 <body>
     <div class="container mt-5">
         <?php
-        $github_username = 'Megha-02'; // Replace with your GitHub username
-        $url = 'https://api.github.com/users/' . $github_username;
+        $github_username = 'GitHub username'; // Replace with your GitHub username
+        $token = 'Access Token'; // Replace with your GitHub Personal Access Token
+
+        $query = <<<EOL
+{
+  user(login: "{$github_username}") {
+    avatarUrl
+    name
+    login
+    followers {
+      totalCount
+    }
+    following {
+      totalCount
+    }
+    repositories {
+      totalCount
+    }
+    location
+    bio
+  }
+}
+EOL;
+
+        $url = 'https://api.github.com/graphql';
+        $data = json_encode(['query' => $query]);
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $data = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: bearer ' . $token));
+        $result = curl_exec($ch);
         curl_close($ch);
 
-        $profile = json_decode($data, true);
+        $response = json_decode($result, true);
 
-        if (isset($profile['login'])) {
-            echo '<img src="' . $profile['avatar_url'] . '" class="avatar">';
+        if (isset($response['data']['user'])) {
+            $profile = $response['data']['user'];
+            echo '<img src="' . $profile['avatarUrl'] . '" class="avatar">';
             echo '<h1>' . $profile['name'] . '</h1>';
             echo '<p>Username: @' . $profile['login'] . '</p>';
-            echo '<p>Followers: ' . $profile['followers'] . '</p>';
-            echo '<p>Following: ' . $profile['following'] . '</p>';
-            echo '<p>Public Repositories: ' . $profile['public_repos'] . '</p>';
+            echo '<p>Followers: ' . $profile['followers']['totalCount'] . '</p>';
+            echo '<p>Following: ' . $profile['following']['totalCount'] . '</p>';
+            echo '<p>Public Repositories: ' . $profile['repositories']['totalCount'] . '</p>';
             echo '<p>Location: ' . $profile['location'] . '</p>';
             echo '<p>Bio: ' . $profile['bio'] . '</p>';
         } else {
